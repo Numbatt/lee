5e4ae5ee1e06ce6f800ea78eb09633213379e618
Numbatt <86483522+Numbatt@users.noreply.github.com>
2023-02-11 22:25:05 -0600

    Lee's Valentine Request

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function fireworks(){
+    var div = document.createElement("div");
+    div.className = "firework";
+    document.getElementsByTagName('body')[0].appendChild(div);
+    document.getElementsByTagName('body')[1].appendChild(div);
+    document.getElementsByTagName('body')[2].appendChild(div);
+}
